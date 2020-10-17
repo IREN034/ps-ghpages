ef503379b81b5cea6c482f5b495d9aadba12b812
iren034 <iren034@gmail.com>
2020-10-17 13:29:47 +0800

    Updates

---
+++ precache-manifest.c92dc6d25bf7766d1650bca75ade418a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded57ced7e636d0eb5ef8f0459bcbc49",
+    "url": "/ps-ghpages/index.html"
+  },
+  {
+    "revision": "b530a96cbcc7d9d1cb72",
+    "url": "/ps-ghpages/static/css/main.ece0a4a3.chunk.css"
+  },
+  {
+    "revision": "46dfee832a4bb4eb4402",
+    "url": "/ps-ghpages/static/js/2.30df0218.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ps-ghpages/static/js/2.30df0218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b530a96cbcc7d9d1cb72",
+    "url": "/ps-ghpages/static/js/main.0a964eff.chunk.js"
+  },
+  {
+    "revision": "9ced3ba9016398df7d0e",
+    "url": "/ps-ghpages/static/js/runtime-main.ba75c3a6.js"
+  }
+]);
